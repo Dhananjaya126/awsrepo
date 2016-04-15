cd8da637c15cfb39437511dada848d87b6256b84
Dhananjaya <dhananjaya.126@gmail.com>
2016-04-15 17:09:22 +0530

    tomcat related changes

--- testproject/scripts/start_server.sh
+++ testproject/scripts/start_server.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 # Start web server
-sudo service httpd start
+tomcat_pid() {
+  echo `ps aux | grep org.apache.catalina.startup.Bootstrap | grep -v grep | awk '{ print $2 }'`
+}
+pid=$(tomcat_pid)
+  if [ -n "$pid" ] 
+  then
+    echo "Tomcat is already running (pid: $pid)"
+  else
+    # Start tomcat
+    echo "Starting tomcat..."
+    sudo service tomcat8 start
+  fi
+
--- testproject/scripts/stop_server.sh
+++ testproject/scripts/stop_server.sh
@@ -1,10 +1,37 @@
 #!/bin/bash
 # Stop web server
-httpd_running=`pgrep httpd`
-if [[ -n  $httpd_running ]]; then
-    sudo service httpd stop
-fi
+
+SHUTDOWN_WAIT=5
+
+tomcat_pid() {
+  echo `ps aux | grep org.apache.catalina.startup.Bootstrap | grep -v grep | awk '{ print $2 }'`
+}
+
+pid=$(tomcat_pid)
+  if [ -n "$pid" ]
+  then
+    echo "Stoping Tomcat"
+    sudo service tomcat8 stop
+
+    let kwait=$SHUTDOWN_WAIT
+    count=0;
+    until [ `ps -p $pid | grep -c $pid` = '0' ] || [ $count -gt $kwait ]
+    do
+      echo -n -e "\nwaiting for processes to exit";
+      sleep 1
+      let count=$count+1;
+    done
+
+    if [ $count -gt $kwait ]; then
+      echo -n -e "\nkilling processes which didn't stop after $SHUTDOWN_WAIT seconds"
+      kill -9 $pid
+      echo  " \nprocess killed manually"
+    fi
+  else
+    echo "Tomcat is not running"
+  fi
 
 
 # Remove existing html pages
-rm -r /var/www/html/*
+#sudo chmod 777 /var/lib/tomcat8/webapps/
+sudo rm -rf /var/lib/tomcat8/webapps/*
